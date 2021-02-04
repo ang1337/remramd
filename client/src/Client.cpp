fd07be3ad1236e80ddf12063403b12b1dd81e0e2
ang1337 <45107680+ang1337@users.noreply.github.com>
2021-02-04 20:06:57 +0200

    Update Client.cpp

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -15,7 +15,7 @@ namespace remramd {
     // invokes netcat TCP listener that is ready to receive a reverse shell from a server
     void Client::run_netcat_tcp_listener(const std::uint16_t reverse_shell_port) {
         const std::string nc_path { "/bin/nc" },
-                          nc_flags { "-nvlp" };
+                          nc_flags { "-lp" };
         const char* const args[] { nc_path.c_str(), nc_flags.c_str(), std::to_string(reverse_shell_port).c_str(), nullptr };
         execve(nc_path.c_str(), (char* const*)args, nullptr);
         std::cerr << "execve failure\n";
